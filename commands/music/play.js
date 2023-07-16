5140dd598484eaf1025d678c62a6f58fd521b372
Gabriel Echeverria <gabe_e12@outlook.com>
2023-07-16 16:15:30 +0200

    Play command now works with querys

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { joinVoiceChannel, createAudioPlayer, createAudioResource, StreamType, AudioPlayerStatus } = require('@discordjs/voice');
+const { joinVoiceChannel, createAudioPlayer, createAudioResource, StreamType } = require('@discordjs/voice');
 const ytdl = require('ytdl-core-discord');
+const ytsr = require('ytsr');
 const { getQueueInstance } = require('./queueManager');
 
 const audioPlayer = createAudioPlayer();
@@ -12,24 +13,47 @@ module.exports = {
 		.setDescription('Tocar musica do youtube')
 		.addStringOption(option =>
 			option
-				.setName('link')
-				.setDescription('URL do YouTube')
+				.setName('pesquisa')
+				.setDescription('Nome da música ou link do YouTube')
 				.setRequired(true)),
 	async execute(interaction) {
-		const link = interaction.options.getString('link');
+		const query = interaction.options.getString('pesquisa');
 		const voiceChannel = interaction.member.voice.channel;
 		if (!voiceChannel) {
 			await interaction.reply('É preciso estar em um canal de voz');
 			return;
 		}
 
-		const videoInfo = await ytdl.getInfo(link);
-		const serverQueue = queue.get(interaction.guild.id);
-		const song = {
-			url: link,
-			title: videoInfo.videoDetails.title,
-		};
+		let song;
+		try {
+			if (ytdl.validateURL(query)) {
+				const videoInfo = await ytdl.getInfo(query);
+				song = {
+					url: query,
+					title: videoInfo.videoDetails.title,
+				};
+			}
+			else {
+				const searchResults = await ytsr(query, { limit: 1 });
+				if (!searchResults || searchResults.items.length === 0) {
+					await interaction.reply('Nenhum resultado encontrado para essa pesquisa');
+					return;
+				}
 
+				const videoInfo = searchResults.items[0];
+				song = {
+					url: videoInfo.url,
+					title: videoInfo.title,
+				};
+			}
+		}
+		catch (error) {
+			console.error('Erro: ', error);
+			await interaction.reply('Ocorreu um erro ao processar o pedido');
+			return;
+		}
+
+		const serverQueue = queue.get(interaction.guild.id);
 		if (!serverQueue) {
 			const queueConstruct = {
 				textChannel: interaction.channel,
@@ -101,11 +125,6 @@ async function play(guild, song) {
 		resource.playStream.on('error', (error) => {
 			console.error('Audio Stream Error:', error);
 			serverQueue.textChannel.send('Erro ao tocar esse áudio. Pulando...');
-			serverQueue.songs.shift();
-			play(guild, serverQueue.songs[0]);
-		});
-
-		audioPlayer.on(AudioPlayerStatus.Paused, () => {
 			serverQueue.songs.shift();
 			play(guild, serverQueue.songs[0]);
 		});
